ba234bb831ed985ddcda192dade5647bee19e9a7
Ujval Desai <ujvaldesai07@gmail.com>
2025-11-11 14:57:27 +0530

    Fix: use node instead of nodemon in start script

--- backend/server.js
+++ backend/server.js
@@ -30,11 +30,11 @@ app.use("/api/v1/blog" ,blogRoute)
  app.use("/api/v1/comment", commentRoute)
 
 
-app.use(express.static(path.join(_dirname, "/frontend/dist")));
+// app.use(express.static(path.join(_dirname, "/frontend/dist")));
 
-app.get(/.*/, (_, res) => {
-  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
-});
+// app.get(/.*/, (_, res) => {
+//   res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+// });
 
 app.listen(PORT, () => {
   connectDB();
